4baec4eaa0ce3840fd59bcf46929672e4ffea665
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-21 00:37:24 +0530

    [akarsh] added: Line Comparision Using Oops

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparisionUsingOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //UC_1CheckLength checklength = new UC_1CheckLength();
+            //checklength.CartesianSystem();
+
+            //UC_2EqualityOfTwoLines equalityOfTwoLines = new UC_2EqualityOfTwoLines();
+            //equalityOfTwoLines.CartesianSystem();
+
+            //UC_3EqualGreaterOrLesser equalGreaterOrLesser = new UC_3EqualGreaterOrLesser();
+            //equalGreaterOrLesser.CartesianSystem();
+
+           
+        }
+    }
+}
